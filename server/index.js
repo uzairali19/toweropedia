7435703843528b50cd9ab0c0b41e81aa69fe1880
Uzair Ali <uzairali19b@gmail.com>
2022-02-10 23:48:08 +0500

    update index.js server PORT

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use('/clients', clientsRouter);
 const CONNECTION =
   'mongodb+srv://uzairali:nAdB7wuwbqqslCFu@moodme-trail.wntop.mongodb.net/toweropedia?retryWrites=true&w=majority';
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 mongoose
   .connect(CONNECTION)
